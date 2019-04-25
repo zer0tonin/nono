050099bde4d421704c3567c74b130d12e91719c7
AliceGG <bouhahah@gmail.com>
2019-04-25 18:34:25 +0200

    feat(client): now using commands

--- nono/client.py
+++ nono/client.py
@@ -1,10 +1,10 @@
 import logging
 
-from discord import Client
+from discord.ext.commands import Bot
 
 logger = logging.getLogger(__name__)
 
-client = Client()
+client = Bot(command_prefix="!")
 
 
 @client.event
@@ -12,10 +12,7 @@ async def on_ready():
     logger.info("We have logged in as {0.user}".format(client))
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+@client.command(name="test")
+async def say_hello(ctx, *args):
+    sentence = " ".join(args)
+    await ctx.send(sentence)
